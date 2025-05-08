642aa9847912142af9af881830548b966e36e121
Correiiaa <tonicorreia2006@gmail.com>
2025-05-08 14:53:35 +0100

    Menus do sistema

--- src/Classes/Login.kt
+++ src/Classes/Login.kt
@@ -39,18 +39,52 @@ class Login {
                 println("1. Ver tarefas")
                 println("2. Registrar venda")
                 println("3. Sair")
+
+                readLine()?.toIntOrNull()?.let { opcao ->
+                    when (opcao) {
+                        1 -> println("Exibindo tarefas...")
+                        2 -> println("Registrando venda...")
+                        3 -> while(true) {
+                            println("Saindo...")
+                            break}
+                        else -> println("Opção inválida.")
+                    }
+                }
             }
             2 -> {
                 println("Menu do Gerente de Armazém:")
                 println("1. Ver estoque")
                 println("2. Adicionar produtos")
                 println("3. Sair")
+
+                readLine()?.toIntOrNull()?.let { opcao ->
+                    when (opcao) {
+                        1 -> println("Exibindo estoque...")
+                        2 -> println("Adicionando produtos...")
+                        3 -> while(true) {
+                            println("Saindo...")
+                            break}
+                        else -> println("Opção inválida.")
+                    }
+                }
             }
             3 -> {
                 println("Menu do Gerente de Funcionários:")
                 println("1. Ver lista de funcionários")
                 println("2. Remover funcionário")
                 println("3. Sair")
+
+                readLine()?.toIntOrNull()?.let { opcao ->
+                    when (opcao) {
+                        1 -> println("Exibindo lista de funcionários...")
+                        2 -> println("Removendo funcionário...")
+                        3 -> while(true) {
+                            println("Saindo...")
+                        break}
+                        else -> println("Opção inválida.")
+
+                    }
+                }
             }
             else -> {
                 println("Função desconhecida. Contate o administrador.")
